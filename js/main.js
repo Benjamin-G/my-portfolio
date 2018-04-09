629ccce2a869df2458ec77464c6ddb3e52c2b8d5
Benjamin Geyer <brgeyer49@gmail.com>
2018-04-09 10:21:05 -0400

    Setup Scss with Webpack

--- js/main.js
+++ js/main.js
@@ -2,5 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import ExampleWork from './example-work'
+import '../styles/main.scss'
 
 ReactDOM.render(<ExampleWork />, document.getElementById('example-work'))
